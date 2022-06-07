1401f766808063718b9eaf8df6921326b8248ecb
郭斌勇 <guobinyong@qq.com>
2022-06-07 19:37:28 +0800

    feat: 增加copy_d_ts方法

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,41 @@
 /**
- * 库的摘要信息
+ * 专门用于构建的相关工具
  * 
  * @remarks
- * 库的说明信息
+ * 本库封装了前端构建时常用的工具、函数等
  * 
  * @packageDocumentation
  */
 
 
-export function main(){
-
-}
+ import {cp} from "fs/promises"
+ import {extname} from "path"
+ 
+ 
+ /**
+  * TypeScript类型声明文件的过滤函数
+  * @param path - 文件路径
+  * @internal
+  */
+ function d_ts_filter(path: string) {
+     return /.d.ts$/.test(path) || extname(path).length === 0
+ }
+ 
+ 
+ /**
+  * 拷贝 TypeScript 的类型声明文件
+  * @remarks
+  * 将 源目录 中 TypeScript 类型声明文件 拷贝到 目标目录下
+  * 
+  * @param src - 源目录
+  * @param dest - 目标目录
+  * @param options - 选项
+  */
+ export const copy_d_ts:typeof cp = function copyTSDeclarationFiles(src,dest,options) {
+     return cp(src,dest,{
+         recursive:true,
+         force:true,
+         filter:d_ts_filter,
+         ...options
+     });
+ }
