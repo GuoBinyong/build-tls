2d32cfab897335e6d5a693bce011dc060dc44129
郭斌勇 <guobinyong@qq.com>
2022-06-08 09:45:31 +0800

    feat: 增加.d.ts文件的逻辑

--- src/index.ts
+++ src/index.ts
@@ -39,3 +39,90 @@
          ...options
      });
  }
+
+
+ 
+
+import {exec} from "node:child_process";
+import process from "node:process"
+
+
+/**
+ * generate_d_ts 的选项
+ */
+export interface Generate_D_TS_Options {
+
+    /**
+     * 是否拷贝项目中已有的 `.d.ts` 文件
+     */
+    copyDTS?:boolean|null;
+
+    /**
+     * 传给 `tsc` 命令的选项
+     */
+    comArg?:string|null;
+
+    /**
+     * 是否要在当前进程将要退出时执行
+     * 
+     * @defaultValue true
+     */
+    onExit?:boolean|null;
+}
+
+
+/**
+ * 生成 TypeScript 类型声明文件
+ * @remarks
+ * 通过 tsc 命令生成 TypeScript 类型声明文件，并且可以拷贝 源目录下的  `.d.ts` 文件到输出目录中。
+ * 
+ * 注意：tsc 命令本身不会拷贝项目中已有的 `.d.ts` 文件。
+ * 
+ * @param src - 源目录
+ * @param dest - 存放生成的 `.d.ts` 的输出目录
+ * @param options - 选项
+ * @returns 操作完成的 Promise
+ */
+export function generate_d_ts(src:string,dest:string,options?:Generate_D_TS_Options|null){
+    const {copyDTS,comArg,onExit = true} = options ?? {};
+    const comArgStr = comArg || "";
+
+    function generate(){
+        let copyPro = Promise.resolve();
+        if (copyDTS){
+            copyPro =  copy_d_ts(src,dest);
+        }
+    
+        const tscPro = new Promise((resolve, reject) =>{
+            exec(`npx tsc --emitDeclarationOnly --outDir ${dest} ${comArgStr}`,function(err,stdout,stderr){
+                if (err) {
+                    console.error(stderr);
+                    reject(err);
+                }else{
+                    console.log(stdout);
+                    resolve(stdout);
+                }
+            });
+        });
+    
+       return Promise.all([copyPro,tscPro]);
+    }
+
+
+    if (onExit){
+       return new Promise(function (resolve){
+            process.on("beforeExit",()=>{
+                if ((process as any).hasGenerated_d_ts){
+                    return resolve(null);
+                }
+                resolve(generate());
+                (process as any).hasGenerated_d_ts = true;
+            });
+        });
+
+    }else{
+        return generate();
+    }
+
+  
+}
